2080a4ee39d2c633fd487a491a81a647e2e741d0
kclmcredit <k.cheong@lmcredit.com>
2026-05-12 09:42:35 -0400

    Initial Commit

---
+++ idling stopper/idling_stopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  idling_stopperApp.swift
+//  idling stopper
+//
+//  Created by Kelvin Cheong on 2026-05-12.
+//
+
+import SwiftUI
+
+@main
+struct idling_stopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ idling stopperTests/idling_stopperTests.swift
@@ -0,0 +1,19 @@
+//
+//  idling_stopperTests.swift
+//  idling stopperTests
+//
+//  Created by Kelvin Cheong on 2026-05-12.
+//
+
+import Testing
+@testable import idling_stopper
+
+struct idling_stopperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
